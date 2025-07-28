32d176ae4bddc968282c35888fc5717098aa8aa4
ARYA SANGAJI <aryasangajiarya@gmail.com>
2025-07-28 16:28:22 +0700

    modify by adding another register

--- modbus_reader.py
+++ modbus_reader.py
@@ -14,6 +14,10 @@ MODBUS_IP = os.getenv('MODBUS_IP')
 MODBUS_PORT = int(os.getenv('MODBUS_PORT', 502))
 API_URL = os.getenv('API_URL')
 REGISTER_TEMP = int(os.getenv('REGISTER_TEMP'))
+REGISTER_SEAM_LEFT = int(os.getenv('REGISTER_SEAM_LEFT'))
+REGISTER_SEAM_RIGHT = int(os.getenv('REGISTER_SEAM_RIGHT'))
+REGISTER_LEVEL = int(os.getenv('REGISTER_LEVEL'))
+REGISTER_SET_TEMP = int(os.getenv('REGISTER_SET_TEMP'))
 REGISTER_BTN = int(os.getenv('REGISTER_BTN'))
 BIT_POS_BTN = int(os.getenv('BIT_POS_BTN'))
 READ_INTERVAL_SECONDS = 2 # Interval pembacaan data
@@ -35,15 +39,38 @@ def modbus_reader_thread():
         try:
             client.connect()
             
-            # Baca register suhu
             temp_response = client.read_holding_registers(REGISTER_TEMP, count=1, slave=1)
-            if temp_response.isError():
-                print(f"[Reader] Error membaca register suhu: {temp_response}")
+            seamL_response = client.read_holding_registers(REGISTER_SEAM_LEFT, count=1, slave=1)
+            seamR_response = client.read_holding_registers(REGISTER_SEAM_RIGHT, count=1, slave=1)
+            level_response = client.read_holding_registers(REGISTER_LEVEL, count=1, slave=1)
+            setTemp_response = client.read_holding_registers(REGISTER_SET_TEMP, count=1, slave=1)
+            
+            if temp_response.isError() :
+                print(f"[Reader] Error membaca register suhu : {temp_response}")
                 continue # Coba lagi di iterasi berikutnya
             
             temperature = temp_response.registers[0] / 10.0
             
-            # Baca register tombol
+            if seamL_response.isError() :
+                print(f"[Reader] Error membaca register seam kiri: {seamL_response}")
+                continue # Coba lagi di iterasi berikutnya
+            seam_left = seamL_response.registers[0] 
+
+            if seamR_response.isError() :
+                print(f"[Reader] Error membaca register seam kanan: {seamR_response}")
+                continue # Coba lagi di iterasi berikutnya
+            seam_right = seamR_response.registers[0] 
+
+            if level_response.isError() :
+                print(f"[Reader] Error membaca register level: {level_response}")
+                continue # Coba lagi di iterasi berikutnya
+            level = level_response.registers[0] 
+
+            if setTemp_response.isError() :
+                print(f"[Reader] Error membaca register set temperature: {setTemp_response}")
+                continue # Coba lagi di iterasi berikutnya
+            set_temp = setTemp_response.registers[0] / 10.0
+
             btn_response = client.read_holding_registers(REGISTER_BTN, count=1, slave=1)
             if btn_response.isError():
                 print(f"[Reader] Error membaca register tombol: {btn_response}")
@@ -53,7 +80,7 @@ def modbus_reader_thread():
             button_status = bool((register_value >> BIT_POS_BTN) & 1)
 
             # Send data yang berhasil dibaca ke dalam antrian
-            data_to_send = {"temperature": temperature, "button_on": button_status}
+            data_to_send = {"temperature": temperature, "seam_left": seam_left, "seam_right": seam_right, "level": level,"set_temp": set_temp, "button_on": button_status}
             data_queue.put(data_to_send)
             print(f"[Reader] Data dibaca dan dimasukkan ke antrian: {data_to_send}")
 
